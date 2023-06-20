052fe53f95ec3166266bdeaee636f42ce979a829
Tan203 <1540684326@qq.com>
2023-06-20 14:24:06 +0800

    version 1.2

--- src/main/java/com/javafx/demo/Main.java
+++ src/main/java/com/javafx/demo/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("第一版本");
         System.out.println("第二版本");
+        System.out.println("第二版本分支：javafx");
     }
 }
